ead5440cf90dec70b3bc450a36581d2aa2274b63
Dulik-M <dulikmarcin03@gmail.com>
2025-11-15 13:34:56 +0100

    new route

--- Frontend/my-app/app/routes.ts
+++ Frontend/my-app/app/routes.ts
@@ -2,5 +2,6 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
     index("routes/login.tsx"),
-    route("home", "routes/home.tsx"),
+    route("home", "routes/home.tsx"), 
+    route("register", "routes/register.tsx"),
 ] satisfies RouteConfig;
